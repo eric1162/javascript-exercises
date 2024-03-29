20ec8ea703ddda8fc883a4201c3050b8d34ea72e
eeeeeric <111892122+eric1162@users.noreply.github.com>
2024-03-29 18:18:12 +0100

    Solved 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fib = [0, 1];
+    if(Number.parseInt(index) === 0){
+        return fib[0];
+    }else if(Number.parseInt(index) < 0){
+        return "OOPS";
+    }
+    let res;
+    for(let i = 1; i < Number.parseInt(index); i++){
+        fib.push(fib[i-1] + fib[i]);
+    }
 
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line
