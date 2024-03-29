38f49c42fd8f9955bee8c9bc1ce045fde9edc088
eeeeeric <111892122+eric1162@users.noreply.github.com>
2024-03-29 18:02:59 +0100

    Solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let phrase2 = "";
+
+    for(let i = 0; i < phrase.length; i++){
+        if (
+            !(phrase.charCodeAt(i) >= 65 && phrase.charCodeAt(i) <= 90) && // A-Z
+            !(phrase.charCodeAt(i) >= 97 && phrase.charCodeAt(i) <= 122) && // a-z
+            !(phrase.charCodeAt(i) >= 48 && phrase.charCodeAt(i) <= 57) // 0-9
+        ){
+            phrase = phrase.slice(0, i) + phrase.slice(i+1);
+            i--;
+        }
+    }
+
+    phrase = phrase.toLowerCase();
+
+    for(let i = phrase.length; i >= 0; i--){
+        phrase2 += phrase.charAt(i);
+    }
+
+    return phrase2 == phrase ? true : false;
 
 };
 
